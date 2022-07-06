3345165486a64824997d763023d6b5ec56d4d698
Joshua Maffuccio <joshuamaffuccio@gmail.com>
2022-07-06 15:07:13 -0700

    Add create handler

--- stacks/index.js
+++ stacks/index.js
@@ -1,10 +1,11 @@
 import { App } from "@serverless-stack/resources";
 import { StorageStack } from "./StorageStack";
+import { ApiStack } from "./ApiStack";
 
 /**
  * @param {App} app
  */
-export default function (app) {
+ export default function main(app) {
   app.setDefaultFunctionProps({
     runtime: "nodejs16.x",
     srcPath: "services",
@@ -12,5 +13,5 @@ export default function (app) {
       format: "esm",
     },
   });
-  app.stack(StorageStack);
+  app.stack(StorageStack).stack(ApiStack);
 }
